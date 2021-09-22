1d300bf1ee19a26ed6ab88086e712458495ec511
MathiasDeHerdt <mathias.de.herdt@student.howest.be>
2021-09-22 11:34:09 +0200

    Reverse DNS Lookup Zone

---
+++ DC1/5.ReverseDNSLookupZone.ps1
@@ -0,0 +1,6 @@
+﻿# networkID is het subnet waar de reverse zone op komt
+# forest wordt het op heel de forest gekent, bij Domain enkel op het domain
+
+Add-DnsServerPrimaryZone -NetworkID "192.168.1.0/24" -ReplicationScope "Forest"
+
+Get-DnsServerZone
