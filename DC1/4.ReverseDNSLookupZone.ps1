c3fa960659718c0dd1bc94e1b4411958974f7a54
MathiasDeHerdt <mathias.de.herdt@student.howest.be>
2021-09-27 15:24:09 +0200

    working version

--- DC1/4.ReverseDNSLookupZone.ps1
+++ DC1/4.ReverseDNSLookupZone.ps1
@@ -1,11 +1,10 @@
-﻿$NetworkSubnet = "192.168.1.0/24"
-$ZoneFile = "1.168.192.in-addr.arpa.dns"
+﻿$ZoneFile = "1.168.192.in-addr.arpa.dns"
 
 $IP = "192.168.1.2"
 $MaskBits = 24 # This means subnet mask = 255.255.255.0
 $Gateway = "192.168.1.1"
-$Dns1 = "192.168.1.2" # DC1
-$Dns2 = "192.168.1.3" # DC2
+$Dns1 = "192.168.1.2"
+$Dns2 = "192.168.1.3"
 $IPType = "IPv4"
 
 # Retrieve the network adapter that you want to configure
@@ -30,5 +29,5 @@ $adapter | New-NetIPAddress `
 # Configure the DNS client server IP addresses
 $adapter | Set-DnsClientServerAddress -ServerAddresses ($Dns1, $Dns2)
 
-Add-DnsServerPrimaryZone -NetworkId $NetworkSubnet -ReplicationScope Domain
+Add-DnsServerPrimaryZone -NetworkId "192.168.1.0/24" -ReplicationScope Domain
 Register-DnsClient
