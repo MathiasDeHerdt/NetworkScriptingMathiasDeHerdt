9972ef498a1f74846cf859d2f97246363f515de7
MathiasDeHerdt <mathias.de.herdt@student.howest.be>
2021-09-29 10:20:53 +0200

    fixed the script and added control on csv file

--- DC1/8.OU-Structuur.ps1
+++ DC1/8.OU-Structuur.ps1
@@ -7,30 +7,42 @@
 # Not needed, we import everything from the CSV file, this is just an example
 # !!!
 # New-ADOrganizationalUnit -Name "Mijn School" -Path "DC=intranet,DC=mijnschool,DC=be"
+# 
 
 Import-Module activedirectory
 
-#Store the data from the CSV in the $ADOU variable. 
-$ADOU = Import-csv C:\Mathias\GitHubRepoV2\NetworkScriptingMathiasDeHerdt\intranet.mijnschool.be\OU.csv
+# See if the csv file exists
+$csvFile = "C:\Mathias\GitHubRepoV2\NetworkScriptingMathiasDeHerdt\intranet.mijnschool.be\OUs.csv"
 
-#Loop through each row containing user details in the CSV file
+$TestedPath = Test-Path -Path $csvFile -PathType Leaf
+
+echo $TestedPath
+
+if ($TestedPath -like "False"){
+    echo "Stopping the script - File not found"
+    exit 1
+}
+
+# Store the data from the CSV in the $ADOU variable. 
+$ADOU = Import-csv $csvFile
+
+# Loop through each row containing user details in the CSV file
 foreach ($ou in $ADou)
 {
-#Read data from each field in each row and assign the data to a variable as below
+    # Read data from each field in each row and assign the data to a variable as below
 
-$name = $ou.Name
-$path = $ou.Path
-$desc = $ou.Description
-$display = $ou.DisplayName
-$AccidentalDel = 0
+    $name = $ou.Name
+    $path = $ou.Path
+    $desc = $ou.Description
+    $display = $ou.DisplayName
+    $AccidentalDel = 0
 
-
-#Account will be created in the OU provided by the $OU variable read from the CSV file
-New-ADOrganizationalUnit `
--Name $name `
--path $path `
--Description $desc `
--DisplayName $display`
--ProtectedFromAccidentalDeletion $AccidentalDel `
+    #Account will be created in the OU provided by the $OU variable read from the CSV file
+    New-ADOrganizationalUnit `
+    -Name $name `
+    -path $path `
+    -Description $desc `
+    -DisplayName $display`
+    -ProtectedFromAccidentalDeletion $AccidentalDel `
 }
 
