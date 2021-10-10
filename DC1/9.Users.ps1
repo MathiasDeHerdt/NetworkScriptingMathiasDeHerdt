90f9ed190173b50be59bf239a3debc00e4690447
MathiasDeHerdt <mathias.de.herdt@student.howest.be>
2021-10-10 13:47:33 +0200

    create users and fixed user accounts csv file

---
+++ DC1/9.Users.ps1
@@ -0,0 +1,43 @@
+# See if the csv file exists
+$csvFile = "C:\Mathias\GitHubRepoV2\NetworkScriptingMathiasDeHerdt\intranet.mijnschool.be\UserAccounts - Copy.csv"
+
+$TestedPath = Test-Path -Path $csvFile -PathType Leaf
+
+echo $TestedPath
+
+if ($TestedPath -like "False"){
+    echo "Stopping the script - File not found"
+    exit 1
+}
+
+# Store the data from the CSV in the $ADUser variable. 
+$ADUser = Import-csv $csvFile -Delimiter ";"
+
+# Loop through each row containing user details in the CSV file
+foreach ($user in $ADUser)
+{
+    # Read data from each field in each row and assign the data to a variable as below
+    $name = $user.Name
+    $acc_name = $user.SamAccountName
+    $given_name = $user.GivenName
+    $sur_name = $user.Surname
+    $display_name = $user.DisplayName
+    $passwd = $user.AccountPassword
+    $home_drive = $user.HomeDrive
+    $home_directory = $user.HomeDirectory
+    $script_path = $user.ScriptPath
+    $path = $user.Path
+
+    # Create the users from the csv file
+    New-ADUser `
+    -Name $name `
+    -SamAccountName $acc_name `
+    -GivenName $given_name `
+    -Surname $sur_name `
+    -DisplayName $display_name `
+    -AccountPassword $passwd `
+    -HomeDrive $home_drive `
+    -HomeDirectory $home_directory `
+    -ScriptPath $script_path `
+    -Path $path
+}
