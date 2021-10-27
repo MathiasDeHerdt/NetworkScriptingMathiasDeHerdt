278f4886d348d0bb3c6fb82fab8057b04d0b5921
MathiasDeHerdt <mathias.de.herdt@student.howest.be>
2021-10-27 16:49:21 +0200

    start webserver
    and cleaned up some code

--- !PowershellOpdracht2/DC2/RoamingProfile.ps1
+++ !PowershellOpdracht2/DC2/RoamingProfile.ps1
@@ -8,20 +8,33 @@ Invoke-Command -Session $S_DC2 -ScriptBlock {
     # Get all members of the AD Group Secretariaat
     $SecrUsers = Get-ADGroupMember -Identity 'Secretariaat'
 
+    # for every user in group secretariaat do loop through
     $SecrUsers | ForEach-Object {
+
+        # get the username
         $SUser = (Get-ADUser $_).Name
+
+        # make the path
         $UP = $roaming_profile_path + $SUser
+
         if (!(Test-Path $UP)) {
             new-item -Path $UP -ItemType Directory
+
             $acl = Get-ACL -Path $UP
+
             $acl.SetAccessRuleProtection($True, $False)
+
             $acl.Access | %{$acl.RemoveAccessRule($_)}
+
             $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Administrators","FullControl","Allow")
             $acl.SetAccessRule($AccessRule)
+
             $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\SYSTEM","FullControl","Allow")
             $acl.SetAccessRule($AccessRule)
+
             $AccessRule = new-object system.security.AccessControl.FileSystemAccessRule("intranet\$SUser",'modify','Allow')
             $acl.AddAccessRule($AccessRule)
+
             Set-Acl -Path $UP -AclObject $acl
         }
         Set-ADUser -Identity $SUser -ProfilePath $UP    }
